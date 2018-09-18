38a9086e5d627d3c8a57ccc67c5792a02fbfd1ba
Deva Narayanan <devanarayananst@gmail.com>
2018-09-18 19:18:34 +0530

    first push

---
+++ DevBot93/Dialogs/BasicLuisDialog.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace Microsoft.Bot.Sample.LuisBot
+{
+    // For more information about this template visit http://aka.ms/azurebots-csharp-luis
+    [Serializable]
+    public class BasicLuisDialog : LuisDialog<object>
+    {
+        public BasicLuisDialog() : base(new LuisService(new LuisModelAttribute(
+            ConfigurationManager.AppSettings["LuisAppId"], 
+            ConfigurationManager.AppSettings["LuisAPIKey"], 
+            domain: ConfigurationManager.AppSettings["LuisAPIHostName"])))
+        {
+        }
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+
+        {
+            string message = $"Sorry, I did not understand '{result.Query}'. Type 'help' if you need assistance.";
+
+            await context.PostAsync(message);
+
+            context.Wait(this.MessageReceived);
+        }
+
+        // Go to https://luis.ai and create a new intent, then train/publish your luis app.
+        // Finally replace "Greeting" with the name of your newly created intent in the following handler
+        [LuisIntent("Greeting")]
+        public async Task GreetingIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisGreetResult(context, result);
+        }
+
+        [LuisIntent("Cancel")]
+        public async Task CancelIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        [LuisIntent("Help")]
+        public async Task HelpIntent(IDialogContext context, LuisResult result)
+        {
+            await this.ShowLuisResult(context, result);
+        }
+
+        private async Task ShowLuisResult(IDialogContext context, LuisResult result) 
+        {
+            await context.PostAsync($"You have reached {result.Intents[0].Intent}. You said: {result.Query}");
+            context.Wait(MessageReceived);
+        }
+
+        private async Task ShowLuisGreetResult(IDialogContext context, LuisResult result)
+        {
+            var name = context.Activity.From.Name;
+            await context.PostAsync($"Hey {name}");
+            context.Wait(MessageReceived);
+        }
+    }
+}
---
+++ DevBot93/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=devbot93-Web-Deploy.pubxml -p:Password=iNP4gFrrggRF7xJqLdN7Rp9tdYeTiRhjldNdRwdCzN9Tmgffe97spJGMo8mM
+
