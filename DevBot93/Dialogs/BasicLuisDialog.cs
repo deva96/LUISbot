c61b5cbcee3e0f4693c1eb043a62e0fcade22ebc
Deva Narayanan <devanarayananst@gmail.com>
2018-09-21 20:19:15 +0530

    Changes

--- DevBot93/Dialogs/BasicLuisDialog.cs
+++ DevBot93/Dialogs/BasicLuisDialog.cs
@@ -40,7 +40,7 @@ namespace Microsoft.Bot.Sample.LuisBot
         public async Task GreetingIntent(IDialogContext context, LuisResult result)
         {
             var name = context.Activity.From.Name;
-            await context.PostAsync($"Hey {name}");
+            await context.PostAsync($"Hey {name} !!How can i be your assistance?");
             context.Wait(MessageReceived);
         }
 
@@ -54,30 +54,58 @@ namespace Microsoft.Bot.Sample.LuisBot
         public async Task ShippingIntent(IDialogContext context, LuisResult result)
         {
             var message = "";
-            if (result.Entities[0].Type == "builtin.number")
+            try
             {
-                Dictionary<string, string> Status = new Dictionary<string, string>();
-                var OrderId = result.Entities[0].Entity;
-                Status = OrderDetails.Order(OrderId,"shipStatus");
-                message = $"Your Shipment Status is {Status["Status"]}";
+                if (result.Entities != null)
+                {
+                    if (result.Entities[0].Type == "builtin.number")
+                    {
+                        Dictionary<string, string> Status = new Dictionary<string, string>();
+                        var OrderId = result.Entities[0].Entity;
+                        Status = OrderDetails.Order(OrderId, "shipStatus");
+                        message = $"Your Shipment Status is {Status["Status"]}";
+                    }
+                    else if (result.Entities[0].Type == "Address")
+                    {
+                        Dictionary<string, string> Address = new Dictionary<string, string>();
+                        var OrderId = "";
+                        if (result.Entities.Count > 1)
+                            OrderId = result.Entities[1].Entity;
+                        Address = OrderDetails.Order(OrderId, "shipStatus");
+                        if (OrderId != "")
+                        {
+                            if (Address["Status"] == "NotYetShipped")
+                            {
+                                var OrderAddress = OrderDetails.Order(OrderId, "shipAddress");
+                                await context.PostAsync($"Can I Change the address of {OrderId}?\nYour address\n{OrderAddress["Address"]}?");
+                                context.Wait(MessageReceived);
+                                await this.ShowLuisConv(context, result);
+                            }
+                            else if (Address["Status"] == "Order not found")
+                                message = $"Sorry you didnt place an Order {OrderId}";
+                            else
+                                message = $"Sorry,Your order {OrderId} is already shipped";
+                        }
+                    }
+                }
             }
-            else if (result.Entities[0].Type == "Address")
+            catch(Exception e)
             {
-                Dictionary<string, string> Address = new Dictionary<string, string>();
-                var OrderId = "";
-                if(result.Entities.Count > 1)
-                OrderId = result.Entities[1].Entity;
-                Address = OrderDetails.Order(OrderId, "shipStatus");
-                if (OrderId != "" && Address["Status"] == "NotYetShipped")
-                {
-                    message = $"Can I Change the address of {OrderId}?";
-                }
-                else
-                {
-                    message = $"Please tell me Order no?";
-                }
+                message = "Please enter your Order no?";
             }
             await context.PostAsync(message); 
+            context.Wait(MessageReceived);
+        }
+
+        private async Task ShowLuisConv(IDialogContext context, LuisResult result)
+        {
+            var message = result.Query;
+            if (message == "yes" || message == "ok" || message == "y")
+            {
+                await context.PostAsync($"Enter your desired address");
+                context.Wait(MessageReceived);
+            }
+            await context.PostAsync($"Ok What else can i do for you?");
             context.Wait(MessageReceived);
         }
 
---
+++ DevBot93/Model/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuisBot.Model
+{
+    public class Response
+    {
+        public int OrderId { get; set; }
+        public String OrderItem { get; set; }
+        public String ShippingAddress { get; set; }
+        public String Status { get; set; }
+    }
+}
